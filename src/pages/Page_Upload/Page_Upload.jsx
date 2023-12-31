2af8f9571d675f51c19f12ebbb615da9cd81962c
trungchanh2410 <tranletrungchanh12345@gmail.com>
2023-12-31 23:37:38 +0700

    feat: :sparkles: Document Upload page
    Document upload and metadata extract page

--- src/pages/Page_Upload/Page_Upload.jsx
+++ src/pages/Page_Upload/Page_Upload.jsx
@@ -1,21 +1,204 @@
-import { Typography } from "antd"
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import './Page_Upload.css'; // Import the external CSS file
 import Bread from "../../components/Bread/Bread"
-import { useEffect } from "react"
-const Page_Upload = () => {
-    useEffect(() => {
-        async function fetchData() {
-            let raw = await fetch("http://localhost:3000/data/metadata.json")
-            let data = await raw.json()
-            console.log("data: ", data)
+import { Select, Button } from 'antd';
+import {
+    UploadOutlined
+} from '@ant-design/icons';
+import MetadataTab from "./Page_Upload_MetaData"; // Import your MetadataTab component
+
+// Set worker URL to initialize PDF.js
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const PdfUploader = () => {
+    const [metadata, setMetadata] = useState(null); // State to store fetched metadata
+    const [numPages, setNumPages] = useState(null);
+    const [file, setFile] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [scale, setScale] = useState(1);
+    const [showMetadataTab, setShowMetadataTab] = useState(false); // State to manage the visibility of the metadata tab
+    const [inputPage, setInputPage] = useState(""); // State to store the input page number
+    const pagesPerView = 1; // Number of pages to show in a single view
+
+    const onFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+        setNumPages(null); // Reset numPages when a new file is selected
+    };
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
+    const handlePageChange = (newPage) => {
+        if (newPage >= 1 && newPage <= numPages) {
+            setCurrentPage(newPage);
         }
-        fetchData()
-    }, [])
+    };
+
+    const handleZoomIn = () => {
+        setScale(scale + 0.1);
+    };
+
+    const handleZoomOut = () => {
+        setScale(scale - 0.1);
+    };
+
+    const handleFitToWidth = () => {
+        setScale(1);
+    };
+
+    const handleExtract = async () => {
+        let raw = await fetch("http://localhost:3000/data/metadata.json");
+        let data = await raw.json();
+
+        // Set the fetched metadata in the state
+        setMetadata(data.metadata);
+
+        // Show metadata tab when the "Start Extract" button is clicked
+        setShowMetadataTab(true);
+    };
+
+    const handleSave = () => {
+        console.log("Save");
+    };
+
+    const handleExportChange = (value) => {
+        console.log(`selected ${value}`);
+    };
+
+    const handleInputChange = (e) => {
+        setInputPage(e.target.value);
+    };
+
+    const handleGoToPage = () => {
+        const pageNumber = parseInt(inputPage, 10);
+        if (!isNaN(pageNumber) && pageNumber >= 1 && pageNumber <= numPages) {
+            setCurrentPage(pageNumber);
+            setInputPage(""); // Clear the input after navigating to the page
+        }
+    };
+
     return (
         <>
-            <Bread title={"Upload new document"} />
-            <Typography.Text>trung chanh</Typography.Text>
-        </>
-    )
-}
+            <Bread title={"New Document"} />
 
-export default Page_Upload
+            <div className="pdf-viewer-container">
+                <div className="navbar">
+                    <label htmlFor="file-upload" className="custom-file-upload" >
+                        <UploadOutlined />
+                        Choose File
+                    </label>
+                    <input
+                        type="file"
+                        id="file-upload"
+                        onChange={onFileChange}
+                        accept=".pdf"
+                    />
+
+                </div>
+                {file && (
+                    <div className='main-content'>
+                        <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+                            <div>
+                                <div className="nav-controls">
+                                    <div style={{ borderRight: "solid 1px #fff", paddingRight: "2em" }}>
+                                        <button onClick={() => handlePageChange(currentPage - pagesPerView)} disabled={currentPage <= 1}>
+                                            &lt;
+                                        </button>
+                                        <span>
+                                            <input
+                                                type="text"
+                                                defaultvalue={`${currentPage}`}
+                                                onChange={handleInputChange}
+                                                onKeyPress={(e) => {
+                                                    if (e.key === 'Enter') {
+                                                        handleGoToPage();
+                                                    }
+                                                }}
+                                                style={{ width: "2em", height: "2em" }}
+                                            />/{numPages}
+                                        </span>
+                                        <button onClick={() => handlePageChange(currentPage + pagesPerView)} disabled={currentPage + pagesPerView > numPages}>
+                                            &gt;
+                                        </button>
+                                    </div>
+
+                                    {/* <button onClick={handleGoToPage}>Go</button> */}
+                                    <div style={{ marginLeft: "2em" }}>
+                                        <button onClick={handleZoomIn}>
+                                            Zoom In
+                                        </button>
+                                        <button onClick={handleZoomOut}>
+                                            Zoom Out
+                                        </button>
+                                        <button onClick={handleFitToWidth}>
+                                            Fit to Width
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="pdf-content" id="pdf-viewer">
+                                    <Document
+                                        file={file}
+                                        onLoadSuccess={onDocumentLoadSuccess}
+                                    >
+                                        {Array.from(new Array(pagesPerView), (el, index) => (
+                                            <Page
+                                                key={`page_${currentPage + index}`}
+                                                pageNumber={currentPage + index}
+                                                scale={scale}
+                                            />
+                                        ))}
+                                    </Document>
+                                </div>
+                            </div>
+                            {showMetadataTab && (
+                                <MetadataTab metadata={metadata} onClose={() => setShowMetadataTab(false)} />
+                            )}
+                        </div>
+                        {!showMetadataTab ? (
+                            <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+                                <div className='extract-div'>
+                                    <h2>Select OCR template: </h2>
+                                    <Select
+                                        defaultValue="0"
+                                        style={{
+                                            width: 200,
+                                        }}
+                                        onChange={handleExportChange}
+                                        options={[
+                                            {
+                                                value: '0',
+                                                label: 'Phap Luat',
+                                            },
+                                            {
+                                                value: '1',
+                                                label: 'Khoa hoc May tinh',
+                                            },
+                                            {
+                                                value: '2',
+                                                label: 'Something Else',
+                                            },
+                                        ]}
+                                    />
+                                    <Button type="primary" size={"large"} onClick={handleExtract}>
+                                        Start Extract
+                                    </Button>
+                                </div>
+                            </div>
+                        ) : (
+                            <div style={{ display: "flex", width: "90%", justifyContent: "right", marginTop: "1em" }}>
+                                <Button type="primary" size={"large"} onClick={handleSave}>
+                                    Save Document
+                                </Button>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div >
+        </>
+    );
+};
+
+export default PdfUploader;
---
+++ src/pages/Page_Upload/Page_Upload_MetaData.jsx
@@ -0,0 +1,113 @@
+// MetadataTab.js
+
+import React, { useEffect, useState } from 'react';
+import './MetadataTab.css';
+import {
+    PlusOutlined
+} from '@ant-design/icons';
+import {
+    Button
+} from 'antd';
+const MetadataTab = ({ metadata, onClose }) => {
+    const [newKey, setNewKey] = useState('');
+    const [newValue, setNewValue] = useState('');
+    const [thisMetadata, setThisMetadata] = useState(metadata)
+
+    useEffect(() => {
+        console.log(thisMetadata)
+    })
+
+    const handleInputChange = (key, value) => {
+        // Your logic to handle input changes
+        console.log(`Updated value for ${key}: ${value}`);
+    };
+
+    const handleAddPair = () => {
+        // Validate that both key and value are provided
+        if (newKey.trim() === '' || newValue.trim() === '') {
+            alert('Please enter both key and value.');
+            return;
+        }
+
+        // Update the metadata with the new key-value pair
+        const updatedMetadata = [...thisMetadata, { key: newKey, value: newValue }];
+        setThisMetadata(updatedMetadata)
+        // Your logic to handle the updated metadata (e.g., update state or send to parent component)
+        console.log('Updated Metadata:', updatedMetadata);
+
+        // Clear the input fields
+        setNewKey('');
+        setNewValue('');
+    };
+
+    return (
+        <div className="metadata-tab">
+            <h3>OCR Result</h3>
+            {thisMetadata && (
+                <>
+                    <div className="metadata-table">
+                        <div className="column key-column">
+                            {thisMetadata.map(({ key }) => (
+                                <div key={key} className="metadata-item key-item">
+                                    {key}:
+                                </div>
+                            ))}
+                            {newKey && (
+                                <div className="metadata-item key-item">
+                                    {newKey}:
+                                </div>
+                            )}
+                        </div>
+
+                        <div className="column value-column">
+                            {thisMetadata.map(({ key, value }) => (
+                                <div key={key} className="metadata-item value-item">
+                                    <input
+                                        type="text"
+                                        defaultValue={value}
+                                        onChange={(e) => handleInputChange(key, e.target.value)}
+                                    />
+                                </div>
+                            ))}
+                            {newKey && (
+                                <div className="metadata-item value-item">
+                                    <input
+                                        type="text"
+                                        value={newValue}
+                                        onChange={(e) => handleInputChange(newKey, e.target.value)}
+                                    />
+                                </div>
+                            )}
+                            {/* Input fields for adding new key-value pair */}
+
+                        </div>
+
+                    </div>
+                    <h4 style={{ margin: ".5em" }}>Add new metadata:</h4>
+
+                    <div className="metadata-input metadata-item value-item">
+                        <input
+                            type="text"
+                            placeholder="New Key"
+                            value={newKey}
+                            onChange={(e) => setNewKey(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="New Value"
+                            value={newValue}
+                            onChange={(e) => setNewValue(e.target.value)}
+                        />
+                        <Button style={{ padding: 0 }} icon={<PlusOutlined />}
+                            onClick={handleAddPair}
+                        />
+                    </div>
+                </>
+            )}
+            <br />
+            <button style={{ float: "right" }} onClick={onClose}>Close</button>
+        </div>
+    );
+};
+
+export default MetadataTab;
