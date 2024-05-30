4f987bda69c3e17f6d98a53e8246d41383d4d578
namkha1032 <namkha1032@gmail.com>
2024-05-30 10:16:14 +0700

    page size

--- src/context/SearchOptionContext.js
+++ src/context/SearchOptionContext.js
@@ -41,7 +41,7 @@ function SearchOptionReducer(state, action) {
                 domain: '4:6189104e-54a2-4243-81ac-77508424ea24:0',
                 // allOntologies: [],
                 current: 1,
-                pageSize: 10,
+                pageSize: 20,
                 search_scope: 'my',
                 oldQuery: ""
             }
