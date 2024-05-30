dbffe0f03234f0d56291b319aa192d27f17398ef
namkha1032 <namkha1032@gmail.com>
2024-05-30 10:19:56 +0700

    search context

--- src/context/SearchOptionContext.js
+++ src/context/SearchOptionContext.js
@@ -61,7 +61,7 @@ const SearchOptionProvider = (props) => {
             domain: '4:6189104e-54a2-4243-81ac-77508424ea24:0',
             // allOntologies: [],
             current: 1,
-            pageSize: 10,
+            pageSize: 20,
             search_scope: 'my',
             oldQuery: ""
         }
